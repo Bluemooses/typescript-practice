bffc55c6e7f7aad44a7b9127b4a04c5274dbe63f
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2021-01-10 12:37:59 -0600

    installed react-router dom, set up basic nav

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+//Components
+import Homepage from "../Components/Homepage/Homepage";
+import Nav from "../Components/Nav/Nav";
 
 function App() {
   return (
     <div className="App">
-      <p>React App</p>
+      <Router>
+        <Nav />
+        <Homepage />
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Homepage/Homepage.tsx
@@ -0,0 +1,11 @@
+const Homepage = () => {
+  console.log("hi");
+  return (
+    <div className="Homepage">
+      <h1 className="WelcomeGreeting">Welcome to Plants and Typescript</h1>
+      {/* <Nav /> */}
+    </div>
+  );
+};
+
+export default Homepage;
