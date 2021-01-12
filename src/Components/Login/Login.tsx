c1ed75e63c4b01eb77d8d90a6b18f34b6ae6cffe
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2021-01-12 16:40:07 -0600

    Playing around with form setup

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,13 +1,39 @@
 import { Text, FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const [signUpData, setSignUpData] = useState({
+    userName: "",
+    password: "",
+  });
+
   return (
     <>
       <FormControl size="sm" id="login">
         <FormLabel>Login</FormLabel>
-        <Input placeholder="Email Address" />
-        <Input placeholder="Password" />
-        <Button>Login</Button>
+        <Input
+          onChange={(event) => (signUpData.userName = event.target.value)}
+          placeholder="Email Address"
+        />
+        <Input
+          onChange={(event) => (signUpData.password = event.target.value)}
+          placeholder="Password"
+        />
+        <Button
+          onClick={() => {
+            console.log(
+              "click",
+              "username",
+              signUpData.userName,
+              "password",
+              signUpData.password
+            );
+          }}
+        >
+          Login
+        </Button>
       </FormControl>
 
       <Text as="i" fontSize="xs">
