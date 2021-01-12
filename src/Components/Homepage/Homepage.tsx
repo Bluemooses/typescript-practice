fd1d4817d3e87e67c4311ffe9b9076a202b27888
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2021-01-12 13:07:13 -0600

    Wrapped App in Chakra Provider and installed dependencies

--- src/Components/Homepage/Homepage.tsx
+++ src/Components/Homepage/Homepage.tsx
@@ -1,10 +1,10 @@
 const Homepage = () => {
-  console.log("hi");
+  //Render
   return (
     <div className="Homepage">
       <h1 className="WelcomeGreeting">Welcome to Plants and Typescript</h1>
     </div>
-  );
+  ); //End render
 };
 
 export default Homepage;
